d62de0e798dbed9ae4d536fb0cd73d7d4fd52805
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-12-29 16:38:07 -0500

    Common module is updated to version 1.2.4 eureka server is updated to version 1.2.0

--- common/src/main/java/com/fakecompany/common/dto/ImageDto.java
+++ common/src/main/java/com/fakecompany/common/dto/ImageDto.java
@@ -1,11 +1,17 @@
 package com.fakecompany.common.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageDto {
 
     private String id;
